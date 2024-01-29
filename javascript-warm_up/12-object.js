9f0ed0f6d7edcd499c1b58dba3ed8962c9d94336
Mbenka22 <mbenka22@gmail.com>
2024-01-29 18:31:34 +0300

    replaceS

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  myObject.value=89;/**You can use the const keyword to declare a 
+  * constant, but the properties of an 
+  * object declared within const 
+  * can still be modified.   */
+  console.log(myObject);
