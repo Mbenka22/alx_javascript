40de214b1cc894e05081fea5bb722f281640cf45
Mbenka22 <mbenka22@gmail.com>
2024-01-29 16:36:21 +0300

    Multiple

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
-const line1 = "C is fun";
-const line2 = "Python is cool";
+const line1 = "C is fun/n" ;
+const line2 = "Python is cool/n";
 const line3 = "JavaScript is amazing";
 
 console.log(line1, line2, line3);
