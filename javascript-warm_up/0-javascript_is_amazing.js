b28da1303a63826d85a0265526e0d90b35a47664
Mbenka22 <mbenka22@gmail.com>
2024-01-29 16:19:25 +0300

    operator

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,2 +1,7 @@
-const myVar = ("JavaScript is amazing")
+const myVar = ("JavaScript is amazing")//single line comments
 console.log(myVar)
+/*
+   This is a multi-line comment
+   It can span multiple lines
+*/
+/* = an assignment operator */
