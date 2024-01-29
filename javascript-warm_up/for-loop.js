3a685f5c42ff6449a248fd20a5335d0741ca40ae
Mbenka22 <mbenka22@gmail.com>
2024-01-29 18:20:08 +0300

    FOR

---
+++ javascript-warm_up/for-loop.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  /* let i=0 the intialization of the  loop: i is less than 5  the condition to be met during iteration;i++ similar to adding one to i*/
