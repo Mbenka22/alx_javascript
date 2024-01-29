ceeb93dd745ad1c45f95140d253cb7b4a00e001e
Mbenka22 <mbenka22@gmail.com>
2024-01-29 18:13:41 +0300

    language aarray

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines);
+for (const language of languages) {
+  console.log(language);
 }
