193fc8cdce85ae1df9d2fdacd0e6913a58062ba1
Mbenka22 <mbenka22@gmail.com>
2024-01-31 00:20:33 +0300

    Double

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -13,21 +13,7 @@ class Rectangle{
         };
         };
 };
-#!/usr/bin/node
-class Rectangle{
-    constructor(w,h){
-    if (w>0 && h>0){
-        this.width = w;
-        this.height = h;
-    };
-    }
-    print() {
-        const p = 'X'
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        };
-        };
-};
+
 rotate() {
     [this.width, this.height]=[this.height, this.width]
 };
