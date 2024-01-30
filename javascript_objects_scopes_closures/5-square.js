17c735bcc64882c1699bca478840f48bdd242e89
Mbenka22 <mbenka22@gmail.com>
2024-01-31 00:42:43 +0300

    Square

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,8 @@
+//super and importing the class rectangle
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+ };
+ module.exports = Square;
