260beec0cda58c544fd427df9ca6e1756fcdc6a2
Mbenka22 <mbenka22@gmail.com>
2024-01-30 23:46:07 +0300

    FOR LOOP

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -9,3 +9,7 @@ class Rectangle{
 }
 
 module.exports=Rectangle
+/*const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);*/
---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+class Rectangle{
+    constructor(w,h){
+    if (w>0 && h>0)
+        {this.width=w=2;
+    this.height=h=3;}
+        }
+    
+};
+print()
+{
+    const myVar='X';
+    for(let i=0;i<this.height;i++){
+        console.log(myVar.repeat(this.width));
+    }
+}
+
+module.exports=Rectangle
