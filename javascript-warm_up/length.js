50e42f452ec788bd5301bdb9e15af2310902d0f7
Mbenka22 <mbenka22@gmail.com>
2024-01-30 10:22:34 +0300

    array

---
+++ javascript-warm_up/length.js
@@ -0,0 +1,11 @@
+//length checks the length of the array:array.length
+var array=[0,1,2,3,4,5,6,7,8,9];
+var arrayLen=array.length;
+console.log(arrayLen);
+//indexof
+var array2 = ['Marion', 'Glory', 'Arbel', 'Elian', 'Dani', 'Ephraim'];
+
+// Use indexOf method on the array to find the index of 'Arbel'
+var indexOfArbel = array2.indexOf('Arbel');
+
+console.log(indexOfArbel);
