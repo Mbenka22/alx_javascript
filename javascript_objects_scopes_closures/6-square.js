87b4bb74b67882d4438ddb4397c463c147f3656f
Mbenka22 <mbenka22@gmail.com>
2024-01-31 01:12:07 +0300

    sQUARE

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
-const SquareA=require('./5-square')
-class Square extends SquareA
-{
-    charPrint(c)
-    {
-        const char =c||'X'
-        for (let i=0;i<this.height;i++){
-            console.log(char.repeat(this.width))
-        };
-    };
-};
+#!/usr/bin/node
+const SquareA = require('./5-square');
+class Square extends SquareA {
+  charPrint(c){
+    const char = c || 'X'
+    for (let i= 0; i< this.height;i++)
+    console.log(char.repeat(this.width));
+  };
+ 
+ };
+ module.exports = Square;
+ 
