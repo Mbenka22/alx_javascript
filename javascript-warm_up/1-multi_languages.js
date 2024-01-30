e59a571c09c5d8392a4b4d6cd5b7bf14b3e9fabe
Mbenka22 <mbenka22@gmail.com>
2024-01-30 10:36:51 +0300

    multilanguage

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log(lines)
+/*let language1 = "C is fun";
+let language2 = "Python is cool";
+let language3 = "JavaScript is amazing";*/
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("Javascript is amazing");
