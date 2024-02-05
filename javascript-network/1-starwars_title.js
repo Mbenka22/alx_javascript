c8e3e47cc05ad0775f60c4f9e77ea293a3b558e8
Mbenka22 <mbenka22@gmail.com>
2024-02-05 22:46:49 +0300

    starwars api

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+// Function to get Star Wars movie title by episode number
+function getMovieTitleByEpisode(episodeNumber) {
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
+
+    request(apiUrl, (error, response, body) => {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            const movieData = JSON.parse(body);
+            if (movieData.title) {
+                console.log(`Title of Episode ${episodeNumber}: ${movieData.title}`);
+            } else {
+                console.log(`Movie with Episode ${episodeNumber} not found.`);
+            }
+        }
+    });
+}
+
+// Get movie title based on user input (movie ID)
+const movieId = process.argv[2];
+
+if (!movieId || isNaN(movieId)) {
+    console.error('Please provide a valid movie ID as the first argument.');
+} else {
+    getMovieTitleByEpisode(movieId);
+}
