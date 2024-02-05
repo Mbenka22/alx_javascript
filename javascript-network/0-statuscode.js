92e3d264ce7d1101640c730ad330e89cf7878dd3
Mbenka22 <mbenka22@gmail.com>
2024-02-05 22:39:22 +0300

    network

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,20 +1,19 @@
 const request = require('request');
 
-// Get the URL from the command line arguments
+
 const url = process.argv[2];
 
-// Check if the URL is provided
 if (!url) {
   console.error('Please provide a URL as the first argument');
   process.exit(1);
 }
 
-// Make a GET request
+
 request.get(url, (error, response) => {
   if (error) {
     console.error('Error:', error.message);
   } else {
-    // Display the status code
+
     console.log('code:', response.statusCode);
   }
 });
