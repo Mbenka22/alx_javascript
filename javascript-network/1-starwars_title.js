9ef46b5b10e7153bf63b3eabc52d1aaf1f5c811d
Mbenka22 <mbenka22@gmail.com>
2024-02-06 23:02:07 +0300

    Swapi

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,4 +1,23 @@
+const id =process.argv[2];
 const request = require('request');
 
-const url = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/:${id}'
 
+request.get(url,(error,response,body)=>{
+    if (error){
+        console.error(error.message);
+    }
+    else if (response.statusCode !==200)
+    {
+        console.error(response.statusCode);
+    }
+    else{
+        try{
+            const movieTitle = JSON.parse(body);
+            console.log('${movieTitle.title}');
+        }
+    catch(parseError){
+        console.error(parseError.message);
+    }
+    }
+})
