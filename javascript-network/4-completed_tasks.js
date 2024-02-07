1f5c139de62fa4c34b8c2d0e73ea65319743b040
Mbenka22 <mbenka22@gmail.com>
2024-02-07 10:49:38 +0300

    tasks by user

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,41 +1,25 @@
-const request = require('request')
-const apiURL = 'https://jsonplaceholder.typicode.com/todos'
+const request = require('request');
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-// Function to compute the number of tasks completed by user ID
-function TasksByUser(apiUrl) {
-    request.get(apiUrl, (error, response, body) => {
-        if (error) {
-            console.log('Error:', error);
-            return;
+request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode === 200) {
+      const completed = {};
+      const tasks = JSON.parse(body);
+      
+      for (const i in tasks) {
+        const task = tasks[i];
+        if (task.completed === true) {
+          if (completed[task.userId] === undefined) {
+            completed[task.userId] = 1;
+          } else {
+            completed[task.userId]++;
+          }
         }
-
-        if (response.statusCode !== 200) {
-            console.log('Unexpected status code:', response.statusCode);
-            return;
-        }
-
-        try {
-            const todos = JSON.parse(body);
-            const completedTasksByUser = {};
-
-            // Using a for loop 
-            for (let i = 0; i < todos.length; i++) {
-                const task = todos[i];
-                if (task.completed) {
-                    if (completedTasksByUser[task.userId]) {
-                        completedTasksByUser[task.userId]++;
-                    } else {
-                        completedTasksByUser[task.userId] = 1;
-                    }
-                }
-            }
-
-            // Print users with completed tasks
-            Object.keys(completedTasksByUser).forEach(userId => {
-                console.log(`${completedTasksByUser[userId]}`);
-            });
-        } catch (parseError) {
-            console.log('Parse error:', parseError);
-        }
-    });
-}
+      }
+      console.log(completed);
+    } else {
+      console.log('An error occured: ' + response.statusCode);
+    }
+  });
