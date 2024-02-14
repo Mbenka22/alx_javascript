b09bacbbad4ed175bdbf08e033f1f4fe0db4e257
Mbenka22 <mbenka22@gmail.com>
2024-02-15 00:13:27 +0300

    delete cookies

--- Cookies_local_storage/src/1-index.js
+++ Cookies_local_storage/src/1-index.js
@@ -1,36 +1,20 @@
 function setCookies() {
-    const firstnameInput = document.getElementById('firstname');
-    const emailInput = document.getElementById('email');
+    const firstnameValue = document.getElementById("firstname").value;
+    const emailValue = document.getElementById("email").value;
 
-    const firstname = firstnameInput.value;
-    const email = emailInput.value;
-    function showCookies() {
-        const cookies = document.cookie.split('; ');
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = 'Cookies: ';
+    const expirationDate = new Date();
+    expirationDate.setTime(
+      expirationDate.getTime() + 10 * 24 * 60 * 60 * 1000
+    );
 
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i];
-            if (cookie.startsWith('firstname=') || cookie.startsWith('email=')) {
-                paragraph.innerHTML += cookie.split('=')[1] + ', ';
-            }
-        }
+    document.cookie = `firstname=${firstnameValue}; expires=${expirationDate.toUTCString()}; path=/`;
+    document.cookie = `email=${emailValue}; expires=${expirationDate.toUTCString()}; path=/`;
+  }
 
+  function showCookies() {
+    const cookiesParagraph = document.createElement("p");
+    const cookiesValue = document.cookie;
 
-
-        // Get the current date
-        let now = new Date();
-
-        // Calculate the expiration date 10 days from now
-        let expirationDate = new Date(now.getTime() + 10 * 24 * 60 * 60 * 1000); // 10 days in milliseconds
-
-        // Convert expiration date to UTC string format
-        let expires = "expires=" + expirationDate.toUTCString();
-
-        // Set the cookies with expiration date
-        document.cookie = `firstname=${firstname}; ${expires}; path=/`;
-        document.cookie = `email=${email}; ${expires}; path=/`;
-    }
-    paragraph.innerHTML = paragraph.innerHTML.slice(0, -2);
-    document.body.appendChild(paragraph);
-}
+    cookiesParagraph.innerHTML = `Cookies: ${cookiesValue}`;
+    document.body.appendChild(cookiesParagraph);
+  }
